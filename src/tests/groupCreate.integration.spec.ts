e399da2ddb619df73ce22a5cb6bf5fe0f3d8fd2b
NoamYes <noamiko600@gmail.com>
2022-03-01 16:04:23 +0200

    (fix): group create test name

--- src/tests/groupCreate.integration.spec.ts
+++ src/tests/groupCreate.integration.spec.ts
@@ -1,12 +1,18 @@
 import { connect } from '../shared/infra/mongoose/connection';
 import { MongoMemoryReplSet } from 'mongodb-memory-server';
-import { emptyDB, findByQuery, findOneByQuery } from '../tests/setup/seedUtils';
+import { emptyDB, findByQuery, findOneByQuery } from './setup/seedUtils';
+import { createMock } from 'ts-auto-mock';
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable import/no-mutable-exports */
 import request from 'supertest';
 import * as http from 'http';
 import { start as startServer, app } from '../shared/infra/http/app';
+
+import config from "config";
+
+const sources: string[] = config.get('valueObjects.source.values');
+const es_name_source = sources[1]
 
 let server: http.Server;
 beforeAll(async () => {
@@ -52,32 +58,32 @@ describe('GROUP USECASES', () => {
     
     describe('CREATE Group UseCases ', () => {
 
-        const esGroup = { name: "es_name", source: 'es_name'}
+        const esGroup = { name: es_name_source, source: es_name_source}
 
         it('create a VALID root group es_name', async () => {
             const res = await request(app).post(`/api/groups`).send(esGroup).expect(200)
             expect(Object.keys(res.body).length === 1)
             expect(res.body.id).toBeTruthy()
             esId = res.body.id
-            const foundESGroup = await findOneByQuery('groups', { name: "es_name"})
-            expect(foundESGroup).toEqual(expect.objectContaining({name: 'es_name', source: 'es_name', isLeaf: true}))
+            const foundESGroup = await findOneByQuery('groups', { name: es_name_source})
+            expect(foundESGroup).toEqual(expect.objectContaining({name: es_name_source, source: es_name_source, isLeaf: true}))
         });
 
         it('create a VALID child group nike', async () => {
             const esRes = await request(app).post(`/api/groups`).send(esGroup).expect(200)
-            nikeGroup = { name: "nike", source: 'es_name', directGroup: esRes.body.id}
+            nikeGroup = { name: "nike", source: es_name_source, directGroup: esRes.body.id}
             const res = await request(app).post(`/api/groups`).send(nikeGroup).expect(200)
             expect(Object.keys(res.body).length === 1)
             expect(res.body.id).toBeTruthy()
             foundNikeGroup = await findOneByQuery('groups', { name: "nike"})
-            expect(foundNikeGroup).toMatchObject({name: 'nike', source: 'es_name', isLeaf: true})
-            const foundGroup = await findOneByQuery('groups', { name: "es_name"})
-            expect(foundGroup).toEqual(expect.objectContaining({name: 'es_name', source: 'es_name', isLeaf: false}))
+            expect(foundNikeGroup).toMatchObject({name: 'nike', source: es_name_source, isLeaf: true})
+            const foundGroup = await findOneByQuery('groups', { name: es_name_source})
+            expect(foundGroup).toEqual(expect.objectContaining({name: es_name_source, source: es_name_source, isLeaf: false}))
         });
 
         it('shouldnt create already exists child group nike', async () => {
             const esRes = await request(app).post(`/api/groups`).send(esGroup).expect(200)
-            nikeGroup = { name: "nike", source: 'es_name', directGroup: esRes.body.id}
+            nikeGroup = { name: "nike", source: es_name_source, directGroup: esRes.body.id}
             await request(app).post(`/api/groups`).send(nikeGroup).expect(200)
             const res = await request(app).post(`/api/groups`).send(nikeGroup).expect(400)
             expect(res.body.message).toEqual(expect.stringContaining('already exists'))
@@ -85,14 +91,14 @@ describe('GROUP USECASES', () => {
 
         it('shouldnt delete a es_name group because its not a leaf', async () => {
             const esRes = await request(app).post(`/api/groups`).send(esGroup).expect(200)
-            nikeGroup = { name: "nike", source: 'es_name', directGroup: esRes.body.id}
+            nikeGroup = { name: "nike", source: es_name_source, directGroup: esRes.body.id}
             await request(app).post(`/api/groups`).send(nikeGroup).expect(200)
             const res = await request(app).delete(`/api/groups/`+esRes.body.id).expect(400)
         });
 
         it('should delete nike child group ', async () => {
             const esRes = await request(app).post(`/api/groups`).send(esGroup).expect(200)
-            nikeGroup = { name: "nike", source: 'es_name', directGroup: esRes.body.id}
+            nikeGroup = { name: "nike", source: es_name_source, directGroup: esRes.body.id}
             const nikeRes = await request(app).post(`/api/groups`).send(nikeGroup).expect(200)
             const res = await request(app).delete(`/api/groups/`+nikeRes.body.id).expect(200)                    
             const foundGroup = await findOneByQuery('groups', { name: "nike"})
@@ -107,7 +113,7 @@ describe('GROUP USECASES', () => {
 
         
         it('shouldnt create a group with non exist parent', async () => {
-            nonValidGroup = { name: "es_name", source: 'es_name', directGroup: '123'}
+            nonValidGroup = { name: es_name_source, source: es_name_source, directGroup: '123'}
             const res = await request(app).post(`/api/groups`).send(nonValidGroup).expect(404)
             expect(res.body.message).toEqual(expect.stringContaining('not exist'))
         });
@@ -151,12 +157,12 @@ describe('GROUP USECASES', () => {
 // describe('PUT Group',() =>{
 //     it('move a group', async()=>{
 //         await emptyDB()
-//         const moveGroupFather = { name: "fatherFirst", source: 'es_name'}
-//         const moveGroupFather2 = {name: "fatherSecond", source: 'es_name'}
+//         const moveGroupFather = { name: "fatherFirst", source: es_name_source}
+//         const moveGroupFather2 = {name: "fatherSecond", source: es_name_source}
 //         const res = await insert('groups', moveGroupFather)
 //         const res2 = await insert('groups', moveGroupFather2)
 //         let foundGroupFatherFirst = await findOneByQuery('groups', { name: "fatherFirst"})
-//         const moveGroupSon = { name: "son", source: 'es_name', directGroup: foundGroupFatherFirst._id.toString()}
+//         const moveGroupSon = { name: "son", source: es_name_source, directGroup: foundGroupFatherFirst._id.toString()}
 //         const res3 = await insert('groups', moveGroupSon)
 //         const foundGroupSon = await findOneByQuery('groups', { name: "son"})
 //         const foundGroupFather2 = await findOneByQuery('groups', { name: "fatherSecond"})
@@ -180,12 +186,12 @@ describe('GROUP USECASES', () => {
 // describe('PUT Group',() =>{
 //     it('move a group with different source, NOT VALID!', async()=>{
 //         await emptyDB()
-//         const moveGroupFather = { name: "fatherFirst", source: 'es_name'}
+//         const moveGroupFather = { name: "fatherFirst", source: es_name_source}
 //         const moveGroupFather2 = {name: "fatherSecond", source: 'ads_name'}
 //         const res = await insert('groups', moveGroupFather)
 //         const res2 = await insert('groups', moveGroupFather2)
 //         let foundGroupFatherFirst = await findOneByQuery('groups', { name: "fatherFirst"})
-//         const moveGroupSon = { name: "son", source: 'es_name', directGroup: foundGroupFatherFirst._id.toString()}
+//         const moveGroupSon = { name: "son", source: es_name_source, directGroup: foundGroupFatherFirst._id.toString()}
 //         const res3 = await insert('groups', moveGroupSon)
 //         const foundGroupSon = await findOneByQuery('groups', { name: "son"})
 //         const foundGroupFather2 = await findOneByQuery('groups', { name: "fatherSecond"})
